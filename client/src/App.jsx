856bef84124f459f6c9663b69e4f538cd083aafb
Palakshisingh <singhshagun713@gmail.com>
2025-04-28 21:21:23 +0530

    ViewApplications,ManageJobs,AddJob

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,11 @@ import ApplyJobs from './pages/ApplyJobs'
 import Home from './pages/Home'
 import RecruiterLogin from './components/RecruiterLogin'
 import { AppContext } from './context/AppContext'
+import Dashboard from './pages/Dashboard'
+import AddJob from './pages/AddJob'
+import ManageJobs from './pages/ManageJobs'
+import ViewApplications from './pages/ViewApplications'
+import 'quill/dist/quill.snow.css' 
 function App() {
   const{showRecruiterLogin} =useContext(AppContext)
 
@@ -16,6 +21,11 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/applications' element={<Applications/>}/>
         <Route path='/apply-job/:id' element={<ApplyJobs/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}>
+          <Route path='add-job' element={<AddJob/>}/>
+          <Route path='manage-jobs' element={<ManageJobs/>}/>
+          <Route path='view-applications' element={<ViewApplications/>}/>
+        </Route>
       </Routes>
     </div>
   )
